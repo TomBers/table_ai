de1c699274ad90fb58abd45f5b48770c7845cb6d
TomBers <tjcberman@gmail.com>
2024-11-12 15:32:08 +0000

    got fixed dates working

--- lib/table_ai/data_fix/test_dates.ex
+++ lib/table_ai/data_fix/test_dates.ex
@@ -1,12 +1,10 @@
 defmodule TableAi.DataFix.TestDates do
   def run do
     prompt = "These are the dates: #{dates() |> Enum.join(", ")}."
-    instructions = TableAi.DataFix.SystemInstructions.get()
+    instructions = TableAi.DataFix.SystemInstructions.fix_dates()
 
-    # Regex expressions lead to invalid JSON at first glance.
-    # Shows the complexity of generating DSL like Regex or SQL.
 
-    TableAi.NlpExtract.TransformSteps.get(prompt, instructions)
+    # TableAi.NlpExtract.TransformSteps.get(prompt, instructions)
   end
 
   def dates do
@@ -20,7 +18,15 @@ defmodule TableAi.DataFix.TestDates do
     ]
   end
 
-  def jsn do
-    "[\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"2020-02-25 - on a thursday\", \"2020-02-25\"],\n    \"regex\": ~r/^(\\d{4}-\\d{2}-\\d{2})/\n  },\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"25-02-2020\"],\n    \"regex\": ~r/^(\\d{2}-\\d{2}-\\d{4})/\n  },\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"1st October, 2022\"],\n    \"regex\": ~r/^(\\d{1,2}(st|nd|rd|th) \\w+, \\d{4})/\n  },\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"11/11/24\"],\n    \"regex\": ~r/^(\\d{2}\\/\\d{2}\\/\\d{2})/\n  },\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"Thursday\"],\n    \"regex\": ~r/^\\w+/\n  },\n  {\n    \"method\": \"fix_date\",\n    \"data\": [\"25th February 2021\"],\n    \"regex\": ~r/^(\\d{1,2}(st|nd|rd|th) \\w+ \\d{4})/\n  }\n]"
+  def results do
+    [
+      "2020-02-25",
+      "2020-02-25",
+      "2022-10-01",
+      "2024-11-11",
+      "2021-02-25",
+      "2020-02-25"
+    ]
   end
+
 end
