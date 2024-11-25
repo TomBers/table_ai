74b2655ccb35a6f9b36190a456effd189b2782a4
TomBers <tjcberman@gmail.com>
2024-11-25 10:12:28 +0000

    add test for data fix

--- lib/table_ai/data_fix/auto_fixer.ex
+++ lib/table_ai/data_fix/auto_fixer.ex
@@ -1,15 +1,18 @@
 defmodule TableAi.DataFix.AutoFixer do
   def run_fixer(errors) do
     # Construct Prompt from the error data
-    _prompt = "The errors :" <> Jason.encode!(errors)
-    _instructions = TableAi.DataFix.SystemInstructions.fix_errors()
+    prompt = "The errors :" <> Jason.encode!(errors)
+    instructions = TableAi.DataFix.SystemInstructions.fix_errors()
 
     # Send the prompt to the LLM
-    # {:ok, fixed_errors} = TableAi.NlpExtract.TransformSteps.get(prompt, instructions)
-    # fixed_errors
+    {:ok, fixed_errors} = TableAi.NlpExtract.TransformSteps.get(prompt, instructions)
+    fixed_errors
+  end
+
+  def example_fixer(_errors) do
     [
-      %{"column_index" => 10, "fixed_data" => "2022-03-25", "row_index" => 3},
-      %{"column_index" => 10, "fixed_data" => "2022-01-17", "row_index" => 10}
+      %{"column_index" => 10, "fixed_data" => "2022-03-25", "row_index" => 10},
+      %{"column_index" => 10, "fixed_data" => "2022-01-17", "row_index" => 14}
     ]
   end
 end
---
+++ test/table_ai/data_fix_test.exs
@@ -0,0 +1,54 @@
+defmodule DataFixTest do
+  use ExUnit.Case
+
+  alias TableAi.NlpExtract.{DataLoader, TransformMachine}
+  alias TableAi.DataFix.AutoFixer
+
+  @limit 100
+  # Testing various transformation engine rules for CSV file
+  def load_csv() do
+    file_path = "test/table_ai/test_errors.csv"
+    DataLoader.file(file_path)
+  end
+
+  def filter_date_steps do
+    [
+      %{
+        "column_index" => 10,
+        "column_type" => "date",
+        "from" => "2021-01-01",
+        "method" => "fillter_row_by_range",
+        "to" => "2023-01-01"
+      }
+    ]
+  end
+
+  test "fail" do
+    df = load_csv()
+    res = filter_date_steps()
+
+    range_filter = Enum.find(res, fn step -> step["method"] == "fillter_row_by_range" end)
+
+    errors =
+      TransformMachine.get_errors(df, range_filter["column_index"], range_filter["column_type"])
+      |> Enum.to_list()
+      |> IO.inspect(label: "errors")
+
+    assert length(errors) == 2
+
+    fixed_df =
+      AutoFixer.example_fixer(errors)
+      |> TransformMachine.fix_errors(df)
+
+    fixed_errors =
+      TransformMachine.get_errors(
+        fixed_df,
+        range_filter["column_index"],
+        range_filter["column_type"]
+      )
+      |> Enum.to_list()
+      |> IO.inspect(label: "Fixed errors")
+
+    assert length(fixed_errors) == 0
+  end
+end
