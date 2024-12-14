b9a8ebb90c26eee211eb2e4c4f96d914f33d99e4
TomBers <tjcberman@gmail.com>
2024-12-14 14:36:26 +0000

    start of writing

---
+++ lib/table_ai_web/live/home_live.ex
@@ -0,0 +1,7 @@
+defmodule TableAiWeb.HomeLive do
+  use TableAiWeb, :live_view
+
+  def mount(params, session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/table_ai_web/router.ex
+++ lib/table_ai_web/router.ex
@@ -17,7 +17,7 @@ defmodule TableAiWeb.Router do
   scope "/", TableAiWeb do
     pipe_through :browser
 
-    live "/", TableLive
+    live "/", HomeLive
     live "/file_upload", FileUploadLive
     live "/talk/uploads/:path", TableLive
     live "/file/:path", FileLive
