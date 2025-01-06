0a175756d750840d88a27a92c88c365dd366754e
TomBers <tjcberman@gmail.com>
2025-01-06 11:27:02 +0000

    add test for flow processing

---
+++ test/table_ai/flow_csv_test.exs
@@ -0,0 +1,35 @@
+defmodule TableAi.FlowProcessing.FlowCsvTest do
+  use ExUnit.Case, async: true
+
+  alias Erl2exVendored.Results
+  alias TableAi.FlowProcessing.FlowCsv
+
+  describe "TableAi.FlowProcessing.FlowCsv.process_file/2" do
+    test "it applies transformations on a file" do
+      steps = [
+        %{
+          "column_index" => 6,
+          "column_type" => "int",
+          "from" => 1960,
+          "method" => "fillter_row_by_range",
+          "to" => 1979
+        },
+        %{
+          "column_index" => 4,
+          "column_type" => "float",
+          "from" => 7,
+          "method" => "fillter_row_by_range",
+          "to" => nil
+        },
+        %{"filters" => ["London"], "method" => "filter_row", "row_index" => 1}
+      ]
+
+      file_path = "priv/static/uploads/imdb"
+
+      result = FlowCsv.process_file(file_path, steps) |> IO.inspect(label: "Results")
+
+      assert result.errors == [], "Expected no errors, got: #{inspect(result.errors)}"
+      assert length(result.data) == 12, "Expected 12 row, got: #{inspect(result.data)}"
+    end
+  end
+end
